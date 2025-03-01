67308b99dfc45d727e5d9ab3293bda5cd7833aa5
Gifa Eriyanto <gifa.eriyanto@gmail.com>
2025-03-01 16:13:37 +0800

    feat: laporan protokol

--- apps/admin/src/app/laporan-keuangan/ramadhan/print/page.tsx
+++ apps/admin/src/app/laporan-keuangan/ramadhan/print/page.tsx
@@ -2,6 +2,11 @@
 
 import {
   Box,
+  Card,
+  CardBody,
+  FormControl,
+  FormLabel,
+  Select,
   Table,
   TableContainer,
   Tbody,
@@ -55,6 +60,9 @@ const Page = () => {
       >
     | undefined
   >(undefined)
+  const [printFormat, setPrintFormat] = useState<'common' | 'for-read'>(
+    'common'
+  )
 
   const getWalletSummary = useCallback(
     (walletId: string) => {
@@ -90,18 +98,160 @@ const Page = () => {
     })
   }, [getWalletSummary, walletsData])
 
-  return (
-    <Layout.Body>
-      <VStack align="stretch" spacing={8}>
-        <VStack mb={8}>
-          <Text textStyle={TextStyle.H1}>
-            Laporan Keuangan Ramadhan Masjid Al-Kautsar CitraLand Tallasa City
-          </Text>
-          <Text textStyle={TextStyle.H2}>
-            {dateFormat(new Date(start_date), 'dd MMMM yyyy')} -{' '}
-            {dateFormat(new Date(end_date), 'dd MMMM yyyy')}
-          </Text>
-        </VStack>
+  const renderReport = useMemo(() => {
+    if (printFormat === 'for-read') {
+      return (
+        <>
+          {walletsData.map((wallet) => {
+            const summary = walletSummary?.[wallet.id]
+            if (!summary) {
+              return null
+            }
+            return (
+              <Box key={wallet.id}>
+                <VStack align="stretch" spacing={8}>
+                  <Box>
+                    <Text textStyle={TextStyle.H1} mb={4} textAlign="center">
+                      Pemasukan
+                    </Text>
+                    <TableContainer
+                      bgColor="white"
+                      sx={{
+                        'th, td': {
+                          borderLeft: '1px solid',
+                          borderColor: 'gray.500',
+                          _last: {
+                            borderRight: '1px solid',
+                          },
+                        },
+                        tr: {
+                          _first: {
+                            'th, td': {
+                              borderTop: '1px solid',
+                            },
+                          },
+                        },
+                      }}
+                    >
+                      <Table>
+                        <Tbody>
+                          <Tr fontWeight="bold" bgColor="yellow.100">
+                            <Td>Saldo kas sampai malam lalu</Td>
+                            <Td w="200px" isNumeric>
+                              {currency(summary.balance - summary.comparation)}
+                            </Td>
+                          </Tr>
+                          {financialReportsData
+                            .filter(
+                              (item) =>
+                                item.wallet_id === wallet.id && item.amount >= 0
+                            )
+                            .map((item) => (
+                              <Tr key={item.id}>
+                                <Td>{item.description}</Td>
+                                <Td isNumeric>
+                                  {item.amount >= 0
+                                    ? currency(item.amount)
+                                    : null}
+                                </Td>
+                              </Tr>
+                            ))}
+                        </Tbody>
+                        <Tfoot
+                          fontWeight="bold"
+                          borderTop="1px solid"
+                          borderColor="gray.500"
+                        >
+                          <Tr bgColor="green.50">
+                            <Td>Total pemasukan malam ini</Td>
+                            <Td w="200px" isNumeric color="green.500">
+                              {currency(summary.total_income)}
+                            </Td>
+                          </Tr>
+                        </Tfoot>
+                      </Table>
+                    </TableContainer>
+                  </Box>
+                  <Box>
+                    <Text textStyle={TextStyle.H1} mb={4} textAlign="center">
+                      Pengeluaran
+                    </Text>
+                    <TableContainer
+                      bgColor="white"
+                      sx={{
+                        'th, td': {
+                          borderLeft: '1px solid',
+                          borderColor: 'gray.500',
+                          _last: {
+                            borderRight: '1px solid',
+                          },
+                        },
+                        tr: {
+                          _first: {
+                            'th, td': {
+                              borderTop: '1px solid',
+                            },
+                          },
+                        },
+                      }}
+                    >
+                      <Table>
+                        <Tbody>
+                          {financialReportsData
+                            .filter(
+                              (item) =>
+                                item.wallet_id === wallet.id && item.amount < 0
+                            )
+                            .map((item) => (
+                              <Tr key={item.id}>
+                                <Td>{item.description}</Td>
+                                <Td w="200px" isNumeric>
+                                  {item.amount < 0
+                                    ? currency(item.amount)
+                                    : null}
+                                </Td>
+                              </Tr>
+                            ))}
+                        </Tbody>
+                        <Tfoot
+                          fontWeight="bold"
+                          borderTop="1px solid"
+                          borderColor="gray.500"
+                        >
+                          <Tr bgColor="red.50">
+                            <Td>Total pengeluaran malam ini</Td>
+                            <Td w="200px" isNumeric color="red.500">
+                              {currency(summary.total_expense)}
+                            </Td>
+                          </Tr>
+                        </Tfoot>
+                      </Table>
+                    </TableContainer>
+                  </Box>
+                  <Box textAlign="center" my={8}>
+                    <Text textStyle={TextStyle.H3} mb={4}>
+                      Total saldo saat ini
+                    </Text>
+
+                    <Text
+                      textStyle={TextStyle.H1}
+                      mb={4}
+                      fontSize="xx-large"
+                      color="green.500"
+                    >
+                      {currency(summary.balance)}
+                    </Text>
+                  </Box>
+                </VStack>
+              </Box>
+            )
+          })}
+        </>
+      )
+    }
+
+    return (
+      <>
         {walletsData.map((wallet) => {
           const summary = walletSummary?.[wallet.id]
           return (
@@ -205,6 +355,46 @@ const Page = () => {
             </Box>
           )
         })}
+      </>
+    )
+  }, [financialReportsData, printFormat, walletSummary, walletsData])
+
+  return (
+    <Layout.Body>
+      <Card
+        mb={8}
+        sx={{
+          '@media print': {
+            display: 'none',
+          },
+        }}
+      >
+        <CardBody>
+          <FormControl>
+            <FormLabel>Format cetak</FormLabel>
+            <Select
+              onChange={(e) => {
+                setPrintFormat(e.currentTarget.value as typeof printFormat)
+              }}
+              value={printFormat}
+            >
+              <option value="common">Standard</option>
+              <option value="for-read">Protokol</option>
+            </Select>
+          </FormControl>
+        </CardBody>
+      </Card>
+      <VStack align="stretch" spacing={8}>
+        <VStack mb={8} textAlign="center">
+          <Text textStyle={TextStyle.H1}>
+            Laporan Keuangan Ramadhan Masjid Al-Kautsar CitraLand Tallasa City
+          </Text>
+          <Text textStyle={TextStyle.H2}>
+            {dateFormat(new Date(start_date), 'dd MMMM yyyy')} -{' '}
+            {dateFormat(new Date(end_date), 'dd MMMM yyyy')}
+          </Text>
+        </VStack>
+        {renderReport}
       </VStack>
 
       <Box mt={8}>
